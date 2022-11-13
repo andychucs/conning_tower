b4c422698c70ffdb3268efa303ee5cb31f87e0da
andy <andyzhuaz@outlook.com>
2022-11-13 21:12:19 +0800

    feat: back and refresh

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,13 @@
-
 import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 const String gameUrl =
-    'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854';
+    'www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/';
 
 class ConnTowerApp extends StatefulWidget {
   const ConnTowerApp({Key? key, this.cookieManager}) : super(key: key);
@@ -18,7 +18,8 @@ class ConnTowerApp extends StatefulWidget {
 }
 
 class ConnTowerHomePage extends State<ConnTowerApp> {
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   late WebViewController __controller;
   // const ConnTowerHomePage({Key? key}) : super(key: key);
 
@@ -33,58 +34,65 @@ class ConnTowerHomePage extends State<ConnTowerApp> {
   @override
   Widget build(BuildContext context) {
     // SystemChrome.setPreferredOrientations(
-    //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    //     [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
     return CupertinoApp(
       theme: const CupertinoThemeData(),
       home: Scaffold(
         body: CupertinoPageScaffold(
-          navigationBar: const CupertinoNavigationBar(
-            middle: Text('Conning Tower'),
-            leading: Icon(CupertinoIcons.plus),
-            trailing: Icon(CupertinoIcons.fullscreen),
+          navigationBar: CupertinoNavigationBar(
+            middle: const Text('Conning Tower'),
+            leading: IconButton(
+                onPressed: () async {
+                  if (await __controller.canGoBack()) {
+                  await __controller.goBack();
+                  }
+                },
+                icon: const Icon(CupertinoIcons.back)),
+            trailing: IconButton(
+                onPressed: (){
+                  __controller.reload();
+                },
+                icon: const Icon(CupertinoIcons.refresh)),
           ),
           child: CupertinoPageScaffold(
-                child: SafeArea(
+              child: SafeArea(
                   left: false,
-                    bottom: false,
-                    right: false,
-                    child: WebView(
-                  initialUrl: "http://www.baidu.com",
-                  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  onWebViewCreated: (WebViewController webViewController) {
-                    _controller.complete(__controller = webViewController);
-                  },
-                  onProgress: (int progress) {
-                    print('WebView is loading (progress : $progress%)');
-                  },
-                  javascriptChannels: <JavascriptChannel>{
-                    _toasterJavascriptChannel(context),
-                  },
-                  navigationDelegate: (NavigationRequest request) {
-                    if (request.url.startsWith('https://www.youtube.com/')) {
-                      print('blocking navigation to $request}');
-                      return NavigationDecision.prevent;
-                    }
-                    print('allowing navigation to $request');
-                    return NavigationDecision.navigate;
-                  },
-                  onPageStarted: (String url) {
-                    print('Page started loading: $url');
-                  },
-                  onPageFinished: (String url) {
-                    print('Page finished loading: $url');
-                    setState(() {
-                      if(url.startsWith('https://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/')) {
-                        print('is game origin url');
-                        __controller.runJavascript('''window.open("http:"+gadgetInfo.URL,'_blank');''');
-                      }
-                    });
-
-                  },
-                  gestureNavigationEnabled: true,
-                  backgroundColor: CupertinoColors.extraLightBackgroundGray,
-                ))),
+                  bottom: false,
+                  right: false,
+                  child: WebView(
+                    initialUrl: 'http://$gameUrl',
+                    userAgent:
+                        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController) {
+                      _controller.complete(__controller = webViewController);
+                    },
+                    onProgress: (int progress) {
+                      print('WebView is loading (progress : $progress%)');
+                    },
+                    javascriptChannels: <JavascriptChannel>{
+                      _toasterJavascriptChannel(context),
+                    },
+                    navigationDelegate: (NavigationRequest request) {
+                      print('allowing navigation to $request');
+                      return NavigationDecision.navigate;
+                    },
+                    onPageStarted: (String url) {
+                      print('Page started loading: $url');
+                    },
+                    onPageFinished: (String url) {
+                      print('Page finished loading: $url');
+                      setState(() {
+                        if (url.endsWith(gameUrl)) {
+                          print('is game origin url');
+                          __controller.runJavascript(
+                              '''window.open("http:"+gadgetInfo.URL,'_blank');''');
+                        }
+                      });
+                    },
+                    gestureNavigationEnabled: true,
+                    backgroundColor: CupertinoColors.extraLightBackgroundGray,
+                  ))),
         ),
       ),
     );
