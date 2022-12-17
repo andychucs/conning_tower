bc5e356b759cc24149ca622cca16af9b482188a9
andy <andyzhuaz@outlook.com>
2022-12-17 23:13:58 +0800

    fix: zht

--- lib/generated/intl/messages_zh-Hant.dart
+++ lib/generated/intl/messages_zh-Hant.dart
@@ -34,7 +34,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "AppBottomSafe": MessageLookupByLibrary.simpleMessage("底部上移"),
         "AppClearCache": MessageLookupByLibrary.simpleMessage("刪除緩存"),
         "AppClearCookie": MessageLookupByLibrary.simpleMessage("登出"),
-        "AppHome": MessageLookupByLibrary.simpleMessage("首頁"),
+        "AppHome": MessageLookupByLibrary.simpleMessage("主頁"),
         "AppLeftSideControlsClearCache":
             MessageLookupByLibrary.simpleMessage("已刪除緩存"),
         "AppLeftSideControlsLoad": m0,
