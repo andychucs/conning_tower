3a5ef981d6e5fc6e073da3019bae665c3ea56b14
andy <andyzhuaz@outlook.com>
2022-12-17 23:10:03 +0800

    fix: zht

--- lib/generated/intl/messages_zh-Hant.dart
+++ lib/generated/intl/messages_zh-Hant.dart
@@ -32,11 +32,11 @@ class MessageLookup extends MessageLookupByLibrary {
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "AppBack": MessageLookupByLibrary.simpleMessage("返回"),
         "AppBottomSafe": MessageLookupByLibrary.simpleMessage("底部上移"),
-        "AppClearCache": MessageLookupByLibrary.simpleMessage("刪除Cache"),
+        "AppClearCache": MessageLookupByLibrary.simpleMessage("刪除緩存"),
         "AppClearCookie": MessageLookupByLibrary.simpleMessage("登出"),
         "AppHome": MessageLookupByLibrary.simpleMessage("首頁"),
         "AppLeftSideControlsClearCache":
-            MessageLookupByLibrary.simpleMessage("Cache cleared."),
+            MessageLookupByLibrary.simpleMessage("已刪除緩存"),
         "AppLeftSideControlsLoad": m0,
         "AppLeftSideControlsLogoutFailed":
             MessageLookupByLibrary.simpleMessage("未找到Cookie"),
