15e36cd2dc62f6d679d82975f670d9954ca3e0cc
Angus <anguslaw63631@gmail.com>
2022-12-30 21:33:15 +0800

    fix: zh_Hant translate

--- lib/generated/intl/messages_zh-Hant.dart
+++ lib/generated/intl/messages_zh-Hant.dart
@@ -37,7 +37,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "AppBack": MessageLookupByLibrary.simpleMessage("返回"),
         "AppBottomSafe": MessageLookupByLibrary.simpleMessage("底部上移"),
         "AppClearCache": MessageLookupByLibrary.simpleMessage("刪除緩存"),
-        "AppClearCookie": MessageLookupByLibrary.simpleMessage("登出"),
+        "AppClearCookie": MessageLookupByLibrary.simpleMessage("刪除Cookie"),
         "AppHome": MessageLookupByLibrary.simpleMessage("主頁"),
         "AppLeftSideControlsClearCache":
             MessageLookupByLibrary.simpleMessage("已刪除緩存"),
