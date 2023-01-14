2a752b980f18bf4ab6dde58a9444f17f8acd4a4b
Angus <anguslaw63631@gmail.com>
2023-01-14 13:39:56 +0800

    add tabs in gameinfo_page.dart

--- lib/pages/gameinfo_page.dart
+++ lib/pages/gameinfo_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:settings_ui/settings_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../generated/l10n.dart';
@@ -33,19 +31,51 @@ class GameInfoPageState extends State<GameInfoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, bool innerBoxIsScrolled) {
-        return [
-          CupertinoSliverNavigationBar(
-            largeTitle: Text("Game Info"),
-          ),
-        ];
-      },
-      body: SafeArea(
+    return SafeArea(
         top: false,
         bottom: false,
-        child: Text("Game Info"),
-      ),
-    );
+        child: Scaffold(
+          bottomNavigationBar:
+          BottomNavigationBar(
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            unselectedItemColor: CupertinoColors.inactiveGray,
+            selectedItemColor: Theme.of(context).primaryColor,
+            items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Fleet',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Battle_result',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Mission',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Sortle',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Map HP',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 0),
+              label: 'Airbase',
+            ),
+
+          ],),
+
+          body: const Center(
+            child: Text(
+              'This is the home page',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      );
   }
 }
