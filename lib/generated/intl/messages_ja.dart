0aadcd9952cc1087d69d29983289099c6abec632
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-04-03 16:01:06 +0800

    fix japanese translate
    (cherry picked from commit 44864c2bb4df56e3417e43a67876959852c0e1d4)

--- lib/generated/intl/messages_ja.dart
+++ lib/generated/intl/messages_ja.dart
@@ -33,7 +33,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "AboutButton": MessageLookupByLibrary.simpleMessage("本アプリ\nについて"),
         "AboutContributors": MessageLookupByLibrary.simpleMessage("貢献者"),
         "AboutDescription": MessageLookupByLibrary.simpleMessage(
-            "Conning TowerやBridgeはクロスプラットフォームプレイに対応した艦隊これくしょんの専用ブラウザです。"),
+            "Conning Towerはクロスプラットフォームプレイに対応した艦隊これくしょんの専用ブラウザです。"),
         "AboutVersion": MessageLookupByLibrary.simpleMessage("バージョン"),
         "AlertNotShowAgain": MessageLookupByLibrary.simpleMessage("再表示しない"),
         "AppBack": MessageLookupByLibrary.simpleMessage("戻る"),
@@ -43,12 +43,12 @@ class MessageLookup extends MessageLookupByLibrary {
         "AppForward": MessageLookupByLibrary.simpleMessage("前へ"),
         "AppHome": MessageLookupByLibrary.simpleMessage("ホーム"),
         "AppLeftSideControlsClearCache":
-            MessageLookupByLibrary.simpleMessage("キャッシュを消去が完了しました"),
+            MessageLookupByLibrary.simpleMessage("キャッシュを消去しました"),
         "AppLeftSideControlsLoad": m0,
-        "AppLeftSideControlsLogoutFailed":
-            MessageLookupByLibrary.simpleMessage("Cookieは見つかりません、ログアウトが必要ないかも"),
+        "AppLeftSideControlsLogoutFailed": MessageLookupByLibrary.simpleMessage(
+            "Cookieは見つかりません。すでにログアウトされている可能性があります。"),
         "AppLeftSideControlsLogoutSuccess":
-            MessageLookupByLibrary.simpleMessage("Cookieを消去しました、ログアウトが成功しました"),
+            MessageLookupByLibrary.simpleMessage("Cookieを消去しました。ログアウトに成功しました"),
         "AppLeftSideControlsNotReady":
             MessageLookupByLibrary.simpleMessage("WebViewの準備がまだ整っていません"),
         "AppLeftSideControlsScrollDown":
@@ -71,8 +71,8 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("全画面表示に失敗しました"),
         "FutureAutoAdjustWindowSuccess":
             MessageLookupByLibrary.simpleMessage("全画面表示に成功しました"),
-        "GameMute": MessageLookupByLibrary.simpleMessage("消す"),
-        "GameUnmute": MessageLookupByLibrary.simpleMessage("つける"),
+        "GameMute": MessageLookupByLibrary.simpleMessage("ミュート"),
+        "GameUnmute": MessageLookupByLibrary.simpleMessage("ミュート解除"),
         "KCViewFuncMsgAlreadyGameRedirect":
             MessageLookupByLibrary.simpleMessage("すでにゲームウィンドウが読み込まれています！"),
         "KCViewFuncMsgAutoGameRedirect":
@@ -80,7 +80,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "KCViewFuncMsgGameNavi":
             MessageLookupByLibrary.simpleMessage("ゲームのロード中"),
         "KCViewFuncMsgGameNaviBlock":
-            MessageLookupByLibrary.simpleMessage("ゲームのリフレッシュは遮断されました"),
+            MessageLookupByLibrary.simpleMessage("ゲームのリフレッシュは中断されました"),
         "KCViewFuncMsgNavi": m1,
         "KCViewFuncMsgNaviGameLoadCompleted":
             MessageLookupByLibrary.simpleMessage("ゲームのロードが完了しました"),
@@ -94,10 +94,10 @@ class MessageLookup extends MessageLookupByLibrary {
         "KCViewFuncMsgPageStart": m3,
         "MsgIOSNote": MessageLookupByLibrary.simpleMessage(
             "iOS 15.0以降の端末をお使いの際、長時間DMM GAMESのトップバーと真っ白なページが表示されたままになることがあります。その場合、「ツール-Web-HTTPリダイレクト」を押すことでプレイ可能な状態になります。"),
-        "MsgMuteGame":
-            MessageLookupByLibrary.simpleMessage("リフレッシュするとゲームの音声が消えます。"),
-        "MsgUnmuteGame":
-            MessageLookupByLibrary.simpleMessage("リフレッシュするとゲームの音声が元に復します。"),
+        "MsgMuteGame": MessageLookupByLibrary.simpleMessage(
+            "ミュートします。変更を適用するにはリフレッシュを行ってください。"),
+        "MsgUnmuteGame": MessageLookupByLibrary.simpleMessage(
+            "ミュートを解除します。変更を適用するにはリフレッシュを行ってください。"),
         "SettingsButton": MessageLookupByLibrary.simpleMessage("設定"),
         "SettingsEnableAutoProcess":
             MessageLookupByLibrary.simpleMessage("ゲームの画面を自動的に全画面する"),
@@ -122,7 +122,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "ToolTitleGameScreen": MessageLookupByLibrary.simpleMessage("画面"),
         "ToolTitleGameSound": MessageLookupByLibrary.simpleMessage("ゲームの音声"),
         "ToolUASetting": MessageLookupByLibrary.simpleMessage("UAを編集する"),
-        "ToolUATip": MessageLookupByLibrary.simpleMessage("アプリを再開するは必要です"),
+        "ToolUATip":
+            MessageLookupByLibrary.simpleMessage("変更を適用するにはアプリを再起動してください"),
         "ToolsButton": MessageLookupByLibrary.simpleMessage("ツール")
       };
 }
