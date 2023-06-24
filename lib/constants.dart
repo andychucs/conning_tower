49197ab961ded9c64bd98d3343fd747365d238d2
andy <andy.chu.cs@icloud.com>
2023-06-25 01:22:13 +0800

    fix: use https

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ const kKancolleHeight = 720;
 const kKancolleWidth = 1200;
 const kKancollePixel = kKancolleHeight * kKancolleWidth;
 const String kGameUrl =
-    'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854';
+    'https://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854';
 const String kGoogle = 'https://www.google.com/';
 const String kSafariUA =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15";
