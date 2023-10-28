48312cd15fef73845b640058ebd4c84d2a0dd4d4
andy <andy.chu.cs@icloud.com>
2023-10-28 20:01:02 +0800

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:conning_tower/app.dart';
 import 'package:conning_tower/constants.dart';
+import 'package:conning_tower/firebase_options.dart';
 import 'package:conning_tower/helper.dart';
 import 'package:conning_tower/utils/logger.dart';
 import 'package:conning_tower/utils/notification_util.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
@@ -48,6 +51,18 @@ Future<void> main() async {
   await notification.init();
 
   await init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   SystemChrome.setPreferredOrientations(DeviceOrientation.values).then(
     (value) => runApp(
