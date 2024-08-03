13861bc492c5bf1f666d956306b0859d20c5cad8
andy <andy.chu.cs@icloud.com>
2024-08-03 18:08:42 +0800

    fix: SeaForceBase serialization
    (cherry picked from commit 864ae9d65ed97606d02626750627eac13b504bbb)

--- lib/models/feature/dashboard/kancolle/sea_force_base.dart
+++ lib/models/feature/dashboard/kancolle/sea_force_base.dart
@@ -5,15 +5,18 @@ import 'package:conning_tower/models/data/kcsapi/port/port_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'sea_force_base.freezed.dart';
+part 'sea_force_base.g.dart';
 
 @unfreezed
 class SeaForceBase with _$SeaForceBase {
   const SeaForceBase._();
 
-  factory SeaForceBase({
-    required SeaForceBaseResource resource,
-    required Admiral admiral
-  }) = _SeaForceBase;
+  factory SeaForceBase(
+      {required SeaForceBaseResource resource,
+      required Admiral admiral}) = _SeaForceBase;
+
+  factory SeaForceBase.fromJson(Map<String, dynamic> json) =>
+      _$SeaForceBaseFromJson(json);
 
   void updateAdmiralInfo(PortApiDataApiBasicEntity apiBasic) {
     admiral = admiral.copyWith(
@@ -84,6 +87,9 @@ class SeaForceBaseResource with _$SeaForceBaseResource {
     required int developmentMaterials,
     required int improvementMaterials,
   }) = _SeaForceBaseResource;
+
+  factory SeaForceBaseResource.fromJson(Map<String, dynamic> json) =>
+      _$SeaForceBaseResourceFromJson(json);
 }
 
 @freezed
@@ -97,6 +103,9 @@ class Admiral with _$Admiral {
     required int maxShip,
     required int maxItem,
   }) = _Admiral;
+
+  factory Admiral.fromJson(Map<String, dynamic> json) =>
+      _$AdmiralFromJson(json);
 
   String get rankName {
     try {
